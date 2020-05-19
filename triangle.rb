af4236b8a2026c2925986979cb7dd19c5dfa6a0b
GodfatherSean <zehao.sean.huang@gmail.com>
2020-05-18 23:05:57 -0700

    triangle

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,10 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  return :equilateral if a == b && b == c
+  return :isosceles if a == b || b == c || c == a
+
+  :scalene
 end
 
 # Error class used in part 2.  No need to change this code.
